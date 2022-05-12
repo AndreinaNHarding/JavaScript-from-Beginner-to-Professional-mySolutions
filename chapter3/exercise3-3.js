e430de66d62b67563bfd004658d2cffdda169733
Andreina N. Harding <76184774+AndreinaNHarding@users.noreply.github.com>
2022-05-12 12:32:13 -0700

    Create exercise3-3.js

---
+++ chapter3/exercise3-3.js
@@ -0,0 +1,13 @@
+/*My solution to
+Practice exercise 3.3 
+from JavaScript Beginners to Professional
+Andreina Nunez*/
+
+//Create an array containing 3 values: 1, 2, 3
+let values = [1, 2, 3];
+
+//Nest the original array into a new array 3 times:
+arrayOfValues = [values, values, values];
+
+//Output the value 2 from the from the first array into the console:
+console.log(arrayOfValues[0][1]);
